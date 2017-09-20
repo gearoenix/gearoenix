fcd0847a634754f6f91b78cc4d3f128be71e117d
Hossein Noroozpour <Hossein.Noroozpour@GMail.com>
2017-09-20 19:14:01 +0430

    only for backup purpose

---
+++ gearoenix/vulkan/buffer/vk-buf-buffer.hpp
@@ -0,0 +1,18 @@
+#ifndef GEAROENIX_VULKAN_BUFFER_MANAGER_HPP
+#define GEAROENIX_VULKAN_BUFFER_MANAGER_HPP
+#include "../../core/gc/cr-gc.hpp"
+#include "../vk-linker.hpp"
+namespace gearoenix {
+namespace render {
+    namespace buffer {
+        class Buffer;
+        class SubBuffer;
+        class Manager {
+        private:
+        public:
+            Manager();
+        };
+    }
+}
+}
+#endif
