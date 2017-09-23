8fd7eccbf064e5540e58936a93e41f06f6f5d3dd
Hossein Noroozpour <Hossein.Noroozpour@GMail.com>
2017-09-23 16:05:11 +0330

    FBP

---
+++ gearoenix/vulkan/descriptor/vk-des-manager.hpp
@@ -0,0 +1,27 @@
+#ifndef GEAROENIX_RENDER_DESCRIPTOR_MANAGER_HPP
+#define GEAROENIX_RENDER_DESCRIPTOR_MANAGER_HPP
+#include "../vk-linker.hpp"
+namespace gearoenix {
+namespace render {
+    namespace device {
+        class Logical;
+    }
+    namespace descriptor {
+        class Manager {
+        public:
+            const static uint32_t TYPE_COUNT;
+
+        private:
+            device::Logical* logical_device;
+            VkDescriptorPool vulkan_data;
+
+        public:
+            Pool(device::Logical* logical_device);
+            ~Pool();
+            const device::Logical* get_logical_device() const;
+            const VkDescriptorPool& get_vulkan_data() const;
+        };
+    } // namespace descriptor
+} // namespace render
+} // namespace gearoenix
+#endif // GEAROENIX_RENDER_DESCRIPTOR_POOL_HPP
