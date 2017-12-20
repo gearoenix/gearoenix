a7e2f0364979e1457737c837ced8647b63156b3c
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-12-20 12:18:52 +0330

    cube texture in directx11 has been written (not tested yet)

--- gearoenix/dx11/texture/dx11-txt-cube.cpp
+++ gearoenix/dx11/texture/dx11-txt-cube.cpp
@@ -1,60 +1,70 @@
-//#include "gles2-txt-cube.hpp"
-//#ifdef USE_DIRECTX11
-//#include "../../render/texture/rnd-txt-png.hpp"
-//#include "../../system/sys-file.hpp"
-//#include "../../system/sys-log.hpp"
-//#include "../gles2-engine.hpp"
-//
-//#define FACES_COUNT 6
-//
-//gearoenix::gles2::texture::Cube::Cube(system::File* file, Engine* eng, std::shared_ptr<core::EndCaller> end)
-//{
-//    std::vector<std::vector<unsigned char>> img_data(FACES_COUNT);
-//    std::vector<unsigned int> imgw(FACES_COUNT), imgh(FACES_COUNT);
-//    std::vector<core::Offset> img_offs(5);
-//    for (int i = 0; i < 5; ++i) {
-//        file->read(img_offs[i]);
-//    }
-//    render::texture::PNG::decode(file, img_data[0], imgw[0], imgh[0]);
-//    for (int i = 1; i < FACES_COUNT; ++i) {
-//        file->seek((unsigned int)img_offs[i - 1]);
-//        render::texture::PNG::decode(file, img_data[i], imgw[i], imgh[i]);
-//    }
-//    static const GLenum faces[] = {
-//        GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
-//        GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
-//        GL_TEXTURE_CUBE_MAP_POSITIVE_X,
-//        GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-//        GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-//        GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
-//    };
-//    std::function<void()> loadf = [&, imgw, imgh, img_data, end]() -> void {
-//        glGenTextures(1, &texture_object);
-//        glBindTexture(GL_TEXTURE_CUBE_MAP, texture_object);
-//        glTexParameterf(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//        glTexParameterf(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); //GL_MIRRORED_REPEAT
-//        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//        for (int i = 0; i < FACES_COUNT; ++i) {
-//            glTexImage2D(faces[i], 0, GL_RGBA, imgw[i], imgh[i], 0, GL_RGBA, GL_UNSIGNED_BYTE, (void*)img_data[i].data());
-//        }
-//        glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
-//        (void)end;
-//        return;
-//    };
-//    eng->add_load_function(loadf);
-//}
-//
-//gearoenix::gles2::texture::Cube::~Cube()
-//{
-//    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-//    glDeleteTextures(1, &texture_object);
-//}
-//
-//void gearoenix::gles2::texture::Cube::bind(GLenum texture_unit)
-//{
-//    glActiveTexture(texture_unit);
-//    glBindTexture(GL_TEXTURE_CUBE_MAP, texture_object);
-//}
-//
-//#endif
+#include "dx11-txt-cube.hpp"
+#ifdef USE_DIRECTX11
+#include "../../render/texture/rnd-txt-png.hpp"
+#include "../../system/sys-file.hpp"
+#include "../../system/sys-log.hpp"
+#include "../dx11-engine.hpp"
+#include "../../core/cr-static.hpp"
+#include "../dx11-check.hpp"
+
+#define FACES_COUNT 6
+
+gearoenix::dx11::texture::Cube::Cube(system::File* file, Engine* eng, std::shared_ptr<core::EndCaller> end)
+{
+    std::vector<unsigned char> img_data;
+	unsigned int imgw, imgh;
+    std::vector<core::Offset> img_offs(5);
+    for (int i = 0; i < 5; ++i) {
+        file->read(img_offs[i]);
+    }
+    render::texture::PNG::decode(file, img_data, imgw, imgh);
+    for (int i = 1; i < FACES_COUNT; ++i) {
+        file->seek((unsigned int)img_offs[i - 1]);
+		std::vector<unsigned char> tmp_img_data;
+		unsigned int tmpimgw, tmpimgh;
+        render::texture::PNG::decode(file, tmp_img_data, tmpimgw, tmpimgh);
+		if (imgw != tmpimgw || imgw != tmpimgh || img_data.size() != tmp_img_data.size()) {
+			UNEXPECTED;
+		}
+		img_data.insert(img_data.end(), tmp_img_data.begin(), tmp_img_data.end());
+    }
+	D3D11_TEXTURE2D_DESC desc;
+	setz(desc);
+	desc.Width = imgw;
+	desc.Height = imgh;
+	desc.ArraySize = FACES_COUNT;
+	desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	desc.SampleDesc.Count = 1;
+	desc.Usage = D3D11_USAGE_IMMUTABLE;
+	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+	desc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS | D3D11_RESOURCE_MISC_TEXTURECUBE;
+	D3D11_SHADER_RESOURCE_VIEW_DESC sdesc;
+	setz(sdesc);
+	sdesc.Format = desc.Format;
+	sdesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURECUBE;
+	sdesc.TextureCube.MipLevels = -1;
+    eng->add_load_function([&, eng, desc, sdesc, img_data, end]() -> void {
+		D3D11_SUBRESOURCE_DATA subsrcdata;
+		subsrcdata.pSysMem = img_data.data();
+		subsrcdata.SysMemPitch = desc.Width * 4;
+		ID3D11Device* dev = eng->get_device();
+		ID3D11Texture2D* txt = nullptr;
+		GXHRCHK(dev->CreateTexture2D(&desc, &subsrcdata, &txt));
+		GXHRCHK(dev->CreateShaderResourceView(txt, &sdesc, &srv));
+		eng->get_context()->GenerateMips(srv);
+		txt->Release();
+		(void)end;
+	});
+}
+
+gearoenix::dx11::texture::Cube::~Cube()
+{
+	srv->Release();
+}
+
+const ID3D11ShaderResourceView* gearoenix::dx11::texture::Cube::get_shader_resource_view()
+{
+	return srv;
+}
+
+#endif
--- gearoenix/dx11/texture/dx11-txt-cube.hpp
+++ gearoenix/dx11/texture/dx11-txt-cube.hpp
@@ -1,31 +1,31 @@
-//#ifndef GEAROENIX_OPENGL_TEXTURE_CUBE_HPP
-//#define GEAROENIX_OPENGL_TEXTURE_CUBE_HPP
-//#include "../../core/cr-build-configuration.hpp"
-//#ifdef USE_DIRECTX11
-//#include "../../render/texture/rnd-txt-texture-cube.hpp"
-//#include "../gles2.hpp"
-//#include <memory>
-//namespace gearoenix {
-//namespace core {
-//    class EndCaller;
-//}
-//namespace system {
-//    class File;
-//}
-//namespace gles2 {
-//    class Engine;
-//    namespace texture {
-//        class Cube : public render::texture::Cube {
-//        private:
-//            GLuint texture_object;
-//
-//        public:
-//            Cube(system::File* file, Engine* engine, std::shared_ptr<core::EndCaller> end);
-//            ~Cube();
-//            void bind(GLenum texture_unit);
-//        };
-//    }
-//}
-//}
-//#endif
-//#endif
+#ifndef GEAROENIX_DX11_TEXTURE_CUBE_HPP
+#define GEAROENIX_DX11_TEXTURE_CUBE_HPP
+#include "../../core/cr-build-configuration.hpp"
+#ifdef USE_DIRECTX11
+#include "../../render/texture/rnd-txt-texture-cube.hpp"
+#include <d3d11.h>
+#include <memory>
+namespace gearoenix {
+namespace core {
+    class EndCaller;
+}
+namespace system {
+    class File;
+}
+namespace dx11 {
+    class Engine;
+    namespace texture {
+        class Cube : public render::texture::Cube {
+        private:
+			ID3D11ShaderResourceView * srv = nullptr;
+
+        public:
+            Cube(system::File* file, Engine* engine, std::shared_ptr<core::EndCaller> end);
+            ~Cube();
+			const ID3D11ShaderResourceView* get_shader_resource_view();
+        };
+    }
+}
+}
+#endif
+#endif
