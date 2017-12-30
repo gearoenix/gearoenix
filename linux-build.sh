687dae1dd3b255c73a6c5ff8da969e48d512a8c7
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2017-12-30 13:20:42 +0330

    linux-build:: --only a line for showing status--

--- linux-build.sh
+++ linux-build.sh
@@ -12,4 +12,5 @@ find ./ -type f -not -name 'data.gx3d' -delete
 cpps=$(find ../gearoenix/ -iname *.cpp | xargs echo)
 clear
 clear
+echo "Please wait, compiling ..."
 clang++ -pthread -std=c++14 -Ofast -ldl -lSDL2 -lGL $cpps -o out.bin
