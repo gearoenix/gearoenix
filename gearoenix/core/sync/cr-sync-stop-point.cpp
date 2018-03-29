f4d0cefc845220b135280ae13827a55da72b26cd
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2018-03-30 03:05:09 +0430

    sync:: --new synchronization structure add to control the flow of physics kernels--

---
+++ gearoenix/core/sync/cr-sync-stop-point.cpp
@@ -0,0 +1,29 @@
+#include "cr-sync-stop-point.hpp"
+#include "cr-sync-semaphore.hpp"
+
+gearoenix::core::sync::StopPoint::StopPoint(int walkers_count)
+    : sem(new Semaphore())
+    , stoper(walkers_count)
+{
+}
+
+gearoenix::core::sync::StopPoint::~StopPoint()
+{
+    delete sem;
+}
+
+void gearoenix::core::sync::StopPoint::all_reach()
+{
+    counter_lock.lock();
+    counter++;
+    if (counter == stoper) {
+        counter = 0;
+        for (int i = 0; i < stoper; ++i)
+            sem->release();
+        counter_lock.unlock();
+        return;
+    } else {
+        counter_lock.unlock();
+        sem->lock();
+    }
+}
---
+++ gearoenix/core/sync/cr-sync-stop-point.hpp
@@ -0,0 +1,25 @@
+#ifndef GEAROENIX_CORE_SYNC_STOP_POINT_HPP
+#define GEAROENIX_CORE_SYNC_STOP_POINT_HPP
+#include "../cr-build-configuration.hpp"
+#include <mutex>
+
+namespace gearoenix {
+namespace core {
+    namespace sync {
+        class Semaphore;
+        class StopPoint {
+        private:
+            Semaphore* sem;
+            const int stoper;
+            volatile int counter = 0;
+            std::mutex counter_lock;
+
+        public:
+            StopPoint(int walkers_count);
+            ~StopPoint();
+            void all_reach();
+        };
+    }
+}
+}
+#endif
