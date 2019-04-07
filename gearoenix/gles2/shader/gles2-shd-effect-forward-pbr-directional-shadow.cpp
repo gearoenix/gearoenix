4436a8a134ca748b27c2a65e572ebe7afc1771d5
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2019-04-08 00:59:29 +0430

    fbp:: shader:: pbr:: --shader is ok for now--

--- gearoenix/gles2/shader/gles2-shd-effect-forward-pbr-directional-shadow.cpp
+++ gearoenix/gles2/shader/gles2-shd-effect-forward-pbr-directional-shadow.cpp
@@ -43,169 +43,169 @@ const static std::string vertex_shader_code =
 
 const static std::string fragment_shader_code =
 	// precisions
-	"precision highp   float;\n"
-	"precision highp   sampler2D;\n"
-	"precision highp   samplerCube;\n"
+	"precision highp   float;\n" // 1
+	"precision highp   sampler2D;\n" // 2
+	"precision highp   samplerCube;\n" // 3
 	// constant(s)
-	"#define GXPI      3.14159265359\n"
+	"#define GXPI      3.14159265359\n" // 4
 	// material uniform(s)
-	"uniform float     material_alpha;\n"
-	"uniform float     material_alpha_cutoff;\n"
-	"uniform float     material_metallic_factor;\n"
-	"uniform float     material_normal_scale;\n"
-	"uniform float     material_occlusion_strength;\n"
-	"uniform float     material_roughness_factor;\n"
-	"uniform sampler2D material_base_color;\n"
-	"uniform sampler2D material_metallic_roughness;\n"
-	"uniform sampler2D material_normal;\n"
-	"uniform sampler2D material_emissive;\n"
+	"uniform float     material_alpha;\n" // 5
+	"uniform float     material_alpha_cutoff;\n" // 6
+	"uniform float     material_metallic_factor;\n" // 7
+	"uniform float     material_normal_scale;\n" // 8
+	"uniform float     material_occlusion_strength;\n" // 9
+	"uniform float     material_roughness_factor;\n" // 10
+	"uniform sampler2D material_base_color;\n" // 11
+	"uniform sampler2D material_metallic_roughness;\n" // 12
+	"uniform sampler2D material_normal;\n" // 13
+	"uniform sampler2D material_emissive;\n" // 14
 	// scenes uniform(s)
-	"uniform vec3      scene_ambient_light;\n"
-	"uniform vec3      scene_directional_lights_color[" GX_MAX_DIRECTIONAL_LIGHTS_STR "];\n"
-	"uniform vec3      scene_directional_lights_direction[" GX_MAX_DIRECTIONAL_LIGHTS_STR "];\n"
-	"uniform vec4      scene_point_lights_color_min_radius[" GX_MAX_POINT_LIGHTS_STR "];\n"
-	"uniform vec4      scene_point_lights_position_max_radius[" GX_MAX_POINT_LIGHTS_STR "];\n"
+	"uniform vec3      scene_ambient_light;\n" // 15
+	"uniform vec3      scene_directional_lights_color[" GX_MAX_DIRECTIONAL_LIGHTS_STR "];\n" // 16
+	"uniform vec3      scene_directional_lights_direction[" GX_MAX_DIRECTIONAL_LIGHTS_STR "];\n" // 17
+	"uniform vec4      scene_point_lights_color_min_radius[" GX_MAX_POINT_LIGHTS_STR "];\n" // 18
+	"uniform vec4      scene_point_lights_position_max_radius[" GX_MAX_POINT_LIGHTS_STR "];\n" // 19
 	// directional, point
-	"uniform vec2      scene_lights_count;\n"
+	"uniform vec2      scene_lights_count;\n" // 20
 	// samples-count, radius, z-tolerance
-	"uniform vec3      scene_ssao_config;\n"
+	"uniform vec3      scene_ssao_config;\n" // 21
 	// effect uniforms
-	"uniform samplerCube effect_diffuse_environment;\n"
-	"uniform samplerCube effect_specular_environment;\n"
-	"uniform sampler2D effect_ambient_occlusion;\n"
-	"uniform sampler2D effect_shadow_map;\n"
-	"uniform sampler2D effect_brdflut;\n"
+	"uniform samplerCube effect_diffuse_environment;\n" // 22
+	"uniform samplerCube effect_specular_environment;\n" // 23
+	"uniform sampler2D effect_ambient_occlusion;\n" // 24
+	"uniform sampler2D effect_shadow_map;\n" // 25
+	"uniform sampler2D effect_brdflut;\n" // 26
 	// light uniform(s)
-	"uniform vec3      light_color;\n"
-	"uniform vec3      light_direction;\n"
+	"uniform vec3      light_color;\n" // 27
+	"uniform vec3      light_direction;\n" // 28
 	// camera uniform(s)
-	"uniform vec3      camera_position;\n"
+	"uniform vec3      camera_position;\n" // 29
 	// output(s) of vertex shader
-	"varying vec3 out_pos;\n"
-    "varying vec3 out_nrm;\n"
-    "varying vec3 out_tng;\n"
-    "varying vec3 out_btg;\n"
-	"varying vec2 out_uv;\n"
-	"varying vec3 out_light_pos;\n"
+	"varying vec3 out_pos;\n" // 30
+    "varying vec3 out_nrm;\n" // 31
+    "varying vec3 out_tng;\n" // 32
+    "varying vec3 out_btg;\n" // 33
+	"varying vec2 out_uv;\n" // 34
+	"varying vec3 out_light_pos;\n" // 35
 	// Normal Distribution Function Trowbridge-Reitz GGX
-	"float distribution_ggx(const vec3 normal, const vec3 halfway, const float roughness) {\n"
-	"    const float roughness2 = roughness * roughness;\n"
-	"    const float nh = max(dot(normal, halfway), 0.0);\n"
-	"    const float nh2 = nh * nh;\n"
-	"    const float nom = roughness2;\n"
-	"    const float tmpdenom = (nh2 * (roughness2 - 1.0) + 1.0);\n"
-	"    const float denom = GXPI * tmpdenom * tmpdenom;\n"
-	"    return nom / denom;\n"
-	"}\n"
-	"float geometry_schlick_ggx(const float nd, const float roughness) {\n"
-	"    const float r = roughness + 1.0;\n"
-	"    const float k = (r * r) * (1.0 / 8.0);\n"
-	"    const float nom = nd;\n"
-	"    const float denom = (nd * (1.0 - k)) + k;\n"
-	"    return nom / denom;\n"
-	"}\n"
-	"float geometry_smith(const vec3 normal, const vec3 view, const vec3 light, const float roughness) {\n"
-	"    const float nv = max(dot(normal, view), 0.0);\n"
-	"    const float nl = max(dot(normal, light), 0.0);\n"
-	"    const float ggx2 = GFSCHGGX(nv, roughness);\n"
-	"    const float ggx1 = GFSCHGGX(nl, roughness);\n"
-	"    return ggx1 * ggx2;\n"
-	"}\n"
-	"vec3 fresnel_schlick(const float nv, const vec3 f0) {\n"
-	"    const float inv = 1.0 - nv;\n"
-	"    const float inv2 = inv * inv;\n"
-	"    const float inv4 = inv2 * inv2;\n"
-	"    const float inv5 = inv4 * inv;\n"
-	"    return f0 + ((1.0 - f0) * inv5);\n"
-	"}\n"
+	"float distribution_ggx(const vec3 normal, const vec3 halfway, const float roughness) {\n" // 37
+	"    float roughness2 = roughness * roughness;\n" // 38
+	"    float nh = max(dot(normal, halfway), 0.0);\n" // 39
+	"    float nh2 = nh * nh;\n" // 40
+	"    float nom = roughness2;\n" // 41
+	"    float tmpdenom = (nh2 * (roughness2 - 1.0) + 1.0);\n" // 42
+	"    float denom = GXPI * tmpdenom * tmpdenom;\n" // 43
+	"    return nom / denom;\n" // 44
+	"}\n" // 45
+	"float geometry_schlick_ggx(const float nd, const float roughness) {\n" // 46
+	"    float r = roughness + 1.0;\n" // 47
+	"    float k = (r * r) * (1.0 / 8.0);\n" // 48
+	"    float nom = nd;\n" // 49
+	"    float denom = (nd * (1.0 - k)) + k;\n" // 50
+	"    return nom / denom;\n" // 51
+	"}\n" // 52
+	"float geometry_smith(const vec3 normal, const vec3 view, const vec3 light, const float roughness) {\n" // 53
+	"    float nv = max(dot(normal, view), 0.0);\n" // 54
+	"    float nl = max(dot(normal, light), 0.0);\n" // 55
+	"    float ggx2 = geometry_schlick_ggx(nv, roughness);\n" // 56
+	"    float ggx1 = geometry_schlick_ggx(nl, roughness);\n" // 57
+	"    return ggx1 * ggx2;\n" // 58
+	"}\n" // 59
+	"vec3 fresnel_schlick(const float nv, const vec3 f0) {\n" // 60
+	"    float inv = 1.0 - nv;\n" // 61
+	"    float inv2 = inv * inv;\n" // 62
+	"    float inv4 = inv2 * inv2;\n" // 63
+	"    float inv5 = inv4 * inv;\n" // 64
+	"    return f0 + ((1.0 - f0) * inv5);\n" // 64
+	"}\n" // 65
 	// cos_theta is (n dot v)
-	"vec3 fresnel_schlick_roughness(const float nv, const vec3 f0, const float roughness)\n"
-	"{\n"
-	"    const float inv = 1.0 - nv;\n"
-	"    const float inv2 = inv * inv;\n"
-	"    const float inv4 = inv2 * inv2;\n"
-	"    const float inv5 = inv4 * inv;\n"
-	"    return f0 + ((max(vec3(1.0 - roughness), f0) - f0) * inv5);\n"
-	"}\n"
-	"void main()\n"
-	"{\n"
+	"vec3 fresnel_schlick_roughness(const float nv, const vec3 f0, const float roughness)\n" // 66
+	"{\n" // 67
+	"    float inv = 1.0 - nv;\n" // 68
+	"    float inv2 = inv * inv;\n" // 69
+	"    float inv4 = inv2 * inv2;\n" // 70
+	"    float inv5 = inv4 * inv;\n" // 71
+	"    return f0 + ((max(vec3(1.0 - roughness), f0) - f0) * inv5);\n" // 72
+	"}\n" // 73
+	"void main()\n" // 74
+	"{\n" // 75
 	//   material properties
-	"    vec4 tmpv4 = texture2D(material_base_color, out_uv);\n"
-	"    tmpv4.w *= material_alpha;\n"
-	"    const vec4 albedo = tmpv4;\n"
-	"    if(albedo.w < material_alpha_cutoff) discard;\n"
-	"	 tmpv4.xy = texture2D(material_metallic_roughness, out_uv).xy;\n"
-	"    tmpv4.xy *= vec2(material_metallic_factor, material_roughness_factor);\n"
-	"    const float metallic = tmpv4.x;\n"
-	"    const float roughness = tmpv4.y;\n"
+	"    vec4 tmpv4 = texture2D(material_base_color, out_uv);\n" // 76
+	"    tmpv4.w *= material_alpha;\n" // 77
+	"    vec4 albedo = tmpv4;\n" // 78
+	"    if(albedo.w < material_alpha_cutoff) discard;\n" // 79
+	"	 tmpv4.xy = texture2D(material_metallic_roughness, out_uv).xy;\n" // 80
+	"    tmpv4.xy *= vec2(material_metallic_factor, material_roughness_factor);\n" // 81
+	"    float metallic = tmpv4.x;\n" // 82
+	"    float roughness = tmpv4.y;\n" // 83
 	//   TODO: in future maybe I will add ao in here
 	//   input lighting data
-	"    const vec3 normal = mat3(tng, btg, nrm) * ((texture2D(material_normal, out_uv).xyz - 0.5) * 2.0 * material_normal_scale);\n"
-	"    const vec3 view = normalize(camera_position - out_pos);\n"
-	"    const vec3 reflection = reflect(-view, normal);\n"
+	"    vec3 normal = mat3(out_tng, out_btg, out_nrm) * ((texture2D(material_normal, out_uv).xyz - 0.5) * 2.0 * material_normal_scale);\n" // 84
+	"    vec3 view = normalize(camera_position - out_pos);\n" // 85
+	"    vec3 reflection = reflect(-view, normal);\n" // 86
 	//   calculate reflectance at normal incidence; if dia-electric (like plastic) use F0
 	//   of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)
 	//   TODO: in future I must bring builder fresnel factor 0 (the hard coded 0.4) from matrial uniform data
-	"    const vec3 f0 = mix(vec3(0.04), albedo.xyz, metallic);\n"
+	"    vec3 f0 = mix(vec3(0.04), albedo.xyz, metallic);\n" // 87
 	//   reflectance equation
-	"    vec3 lo = vec3(0.0);\n"
+	"    vec3 lo = vec3(0.0);\n" // 88
 	//   computing point lights
-	"    for (int i = 0; i < " GX_MAX_POINT_LIGHTS_STR "; ++i)\n"
-	"    {\n"
+	"    for (int i = 0; i < " GX_MAX_POINT_LIGHTS_STR "; ++i)\n" // 89
+	"    {\n" // 90
 	//       calculate per-light radiance
-	"        const vec3 light_vec = scene_point_lights_position_max_radius[i].xyz - out_pos;\n"
+	"        vec3 light_vec = scene_point_lights_position_max_radius[i].xyz - out_pos;\n" // 91
 	//       TODO: in future consider max and min radius
-	"        const float distance = length(light_vec);\n"
-	"        const float distance_inv = 1.0 / distance;\n"
-	"        const vec3 light_direction = light_vec * distance_inv;\n"
-	"        const vec3 half_vec = normalize(view + light);\n"
-	"        const float attenuation = distance * distance;\n"
-	"        const vec3 radiance = scene_point_lights_position_max_radius[i].xyz * attenuation;\n"
+	"        float distance = length(light_vec);\n" // 92
+	"        float distance_inv = 1.0 / distance;\n" // 93
+	"        vec3 light_direction = light_vec * distance_inv;\n" // 94
+	"        vec3 half_vec = normalize(view + light_direction);\n" // 95
+	"        float attenuation = distance_inv * distance_inv;\n" // 96
+	"        vec3 radiance = scene_point_lights_color_min_radius[i].xyz * attenuation;\n" // 97
 	//       Cook-Torrance BRDF
-	"        const float ndf = distribution_ggx(normal, half_vec, roughness);\n"
-	"        const float geo = geometry_smith(normal, view, light_direction, roughness);\n"
-	"        const vec3 frsn = fresnel_schlick(max(dot(half_vec, view), 0.0), f0);\n"
-	"        const vec3 nominator = ndf * geo * frsn;\n"
+	"        float ndf = distribution_ggx(normal, half_vec, roughness);\n" // 98
+	"        float geo = geometry_smith(normal, view, light_direction, roughness);\n" // 99
+	"        vec3 frsn = fresnel_schlick(max(dot(half_vec, view), 0.0), f0);\n" // 100
+	"        vec3 nominator = ndf * geo * frsn;\n" // 101
 	//       0.001 to prevent divide by zero.
-	"        const float denominator = 4.0 * max(dot(normal, view), 0.0) * max(dot(normal, light_direction), 0.0) + 0.001;\n"
-	"        const vec3 specular = nominator / denominator;\n"
+	"        float denominator = 4.0 * max(dot(normal, view), 0.0) * max(dot(normal, light_direction), 0.0) + 0.001;\n" // 102
+	"        vec3 specular = nominator / denominator;\n" // 103
 	//       kS is equal to Fresnel
-	"        const vec3 ks = frsn;\n"
+	"        vec3 ks = frsn;\n" // 104
 	//       for energy conservation, the diffuse and specular light can't
 	//       be above 1.0 (unless the surface emits light); to preserve this
 	//       relationship the diffuse component (kD) should equal 1.0 - kS.
 	//       multiply kD by the inverse metalness such that only non-metals
 	//       have diffuse lighting, or a linear blend if partly metal (pure metals
 	//       have no diffuse light).
-	"        const vec3 kd = (vec3(1.0) - ks) * (1.0 - metallic);\n"
+	"        vec3 kd = (vec3(1.0) - ks) * (1.0 - metallic);\n" // 105
 	//       scale light by NdotL
-	"        const float nl = max(dot(normal, light_direction), 0.0);\n"
+	"        float nl = max(dot(normal, light_direction), 0.0);\n" // 106
 	//       add to outgoing radiance Lo
 	//       note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
-	"        lo += (kd * albedo / GXPI + specular) * radiance * nl;\n"
-	"    }\n"
+	"        lo += (kd * albedo / GXPI + specular) * radiance * nl;\n" // 107
+	"    }\n" // 108
 	//   ambient lighting (we now use IBL as the ambient term)
-	"    const vec3 frsn = fresnel_schlick_roughness(max(dot(normal, view), 0.0), f0, roughness);\n"
-	"    const vec3 ks = frsn;\n"
-	"    const vec3 kd = (1.0 - ks) * (1.0 - metallic);\n"
-	"    const vec3 irradiance = textureCube(effect_diffuse_environment, normal).rgb;\n"
-	"    const vec3 diffuse = irradiance * albedo;\n"
+	"    vec3 frsn = fresnel_schlick_roughness(max(dot(normal, view), 0.0), f0, roughness);\n" // 109
+	"    vec3 ks = frsn;\n" // 110
+	"    vec3 kd = (1.0 - ks) * (1.0 - metallic);\n" // 111
+	"    vec3 irradiance = textureCube(effect_diffuse_environment, normal).rgb;\n" // 112
+	"    vec3 diffuse = irradiance * albedo;\n" // 113
 	//   sample both the pre-filter map and the BRDF lut and combine them together as per
 	//   the Split-Sum approximation to get the IBL specular part.
-	"    const float MAX_REFLECTION_LOD = 4.0;\n"
-	"    const vec3 prefiltered_color = textureCube(effect_specular_environment, reflection, roughness * MAX_REFLECTION_LOD).rgb;\n"
-	"    const vec2 brdf = texture2D(effect_brdflut, vec2(max(dot(normal, view), 0.0), roughness)).rg;\n"
-	"    const vec3 specular = prefiltered_color * (frsn * brdf.x + brdf.y);\n"
+	"    float MAX_REFLECTION_LOD = 4.0;\n" // 114
+	"    vec3 prefiltered_color = textureCube(effect_specular_environment, reflection, roughness * MAX_REFLECTION_LOD).rgb;\n" // 115
+	"    vec2 brdf = texture2D(effect_brdflut, vec2(max(dot(normal, view), 0.0), roughness)).rg;\n" // 116
+	"    vec3 specular = prefiltered_color * (frsn * brdf.x + brdf.y);\n" // 117
 	//   TODO: add ambient occlusion (* ao);
-	"    const vec3 ambient = kd * diffuse + specular;\n"
-	"    tmpv4.xyz = ambient + lo;\n"
+	"    vec3 ambient = kd * diffuse + specular;\n" // 118
+	"    tmpv4.xyz = ambient + lo;\n" // 119
 	//   HDR tonemapping
-	"    tmpv4.xyz = tmpv4.xyz / (tmpv4.xyz + vec3(1.0));\n"
+	"    tmpv4.xyz = tmpv4.xyz / (tmpv4.xyz + vec3(1.0));\n" // 120
 	//   gamma correct
-	"    color = pow(color, vec3(1.0 / 2.2));\n"
+	"    tmpv4.xyz = pow(tmpv4.xyz, vec3(1.0 / 2.2));\n" // 121
 	//   TODO don't forget gamma correction it can be part of scene uniform data
-	"    gl_FragColor = vec4(color, 1.0);\n"
-	"}";
+	"    gl_FragColor = vec4(tmpv4.xyz, albedo.w);\n" // 122
+	"}"; // 123
 
 gearoenix::gles2::shader::ForwardPbrDirectionalShadow::ForwardPbrDirectionalShadow(const std::shared_ptr<engine::Engine> &e, const core::sync::EndCaller<core::sync::EndCallerIgnore> &c)
 	: Shader(e, c)
@@ -216,32 +216,34 @@ gearoenix::gles2::shader::ForwardPbrDirectionalShadow::ForwardPbrDirectionalShad
 		link();
 		GX_GLES2_THIS_GET_UNIFORM_F(camera_position);
 		GX_GLES2_THIS_GET_UNIFORM_F(camera_vp);
-		GX_GLES2_THIS_GET_UNIFORM_F(effect_ambient_occlusion);
+		// TODO
+		//GX_GLES2_THIS_GET_UNIFORM_F(effect_ambient_occlusion);
 		GX_GLES2_THIS_GET_UNIFORM_F(effect_brdflut);
 		GX_GLES2_THIS_GET_UNIFORM_F(effect_diffuse_environment);
-		GX_GLES2_THIS_GET_UNIFORM_F(effect_shadow_map);
+		// TODO
+		// GX_GLES2_THIS_GET_UNIFORM_F(effect_shadow_map);
 		GX_GLES2_THIS_GET_UNIFORM_F(effect_specular_environment);
-		GX_GLES2_THIS_GET_UNIFORM_F(light_color);
+		/*GX_GLES2_THIS_GET_UNIFORM_F(light_color);
 		GX_GLES2_THIS_GET_UNIFORM_F(light_direction);
-		GX_GLES2_THIS_GET_UNIFORM_F(light_vp_bias);
+		GX_GLES2_THIS_GET_UNIFORM_F(light_vp_bias);*/
 		GX_GLES2_THIS_GET_UNIFORM_F(material_alpha);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_alpha_cutoff);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_base_color);
-		GX_GLES2_THIS_GET_UNIFORM_F(material_emissive);
+		//GX_GLES2_THIS_GET_UNIFORM_F(material_emissive);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_metallic_factor);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_metallic_roughness);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_normal);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_normal_scale);
-		GX_GLES2_THIS_GET_UNIFORM_F(material_occlusion_strength);
+		//GX_GLES2_THIS_GET_UNIFORM_F(material_occlusion_strength);
 		GX_GLES2_THIS_GET_UNIFORM_F(material_roughness_factor);
 		GX_GLES2_THIS_GET_UNIFORM_F(model_m);
-		GX_GLES2_THIS_GET_UNIFORM_F(scene_ambient_light);
-		GX_GLES2_THIS_GET_UNIFORM_F(scene_directional_lights_color);
-		GX_GLES2_THIS_GET_UNIFORM_F(scene_directional_lights_direction);
-		GX_GLES2_THIS_GET_UNIFORM_F(scene_lights_count);
+		// GX_GLES2_THIS_GET_UNIFORM_F(scene_ambient_light);
+		// GX_GLES2_THIS_GET_UNIFORM_F(scene_directional_lights_color);
+		// GX_GLES2_THIS_GET_UNIFORM_F(scene_directional_lights_direction);
+		// GX_GLES2_THIS_GET_UNIFORM_F(scene_lights_count);
 		GX_GLES2_THIS_GET_UNIFORM_F(scene_point_lights_color_min_radius);
 		GX_GLES2_THIS_GET_UNIFORM_F(scene_point_lights_position_max_radius);
-		GX_GLES2_THIS_GET_UNIFORM_F(scene_ssao_config);
+		// GX_GLES2_THIS_GET_UNIFORM_F(scene_ssao_config);
 	});
 	gl::sint texture_index = 0;
 	material_base_color_index = texture_index;
