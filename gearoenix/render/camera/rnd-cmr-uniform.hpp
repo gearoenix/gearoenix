19cd2c2b9c1be21b4a5c007c87f5671e7ea8651f
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2019-04-13 23:48:37 +0430

    fbp:: bug-fix:: camera:: uniform:: --a bug in camera uniform has been found and fixed--

--- gearoenix/render/camera/rnd-cmr-uniform.hpp
+++ gearoenix/render/camera/rnd-cmr-uniform.hpp
@@ -8,13 +8,13 @@ namespace render {
 namespace camera {
 struct Uniform
 {
-    math::Vec4 x_reserved;
-    math::Vec4 y_reserved;
-    math::Vec4 z_reserved;
+    math::Vec4 x_reserved = math::Vec4(1.0f, 0.0f, 0.0f, 0.0f);
+    math::Vec4 y_reserved = math::Vec4(0.0f, 1.0f, 0.0f, 0.0f);
+    math::Vec4 z_reserved = math::Vec4(0.0f, 0.0f, 1.0f, 0.0f);
     /// far is negative
-    math::Vec4 position_far;
+    math::Vec4 position_far = math::Vec4(0.0f, 0.0f, 0.0f, -100.0f);
     /// near is negative
-    math::Vec4 near_aspect_ratio_reserved;
+    math::Vec4 near_aspect_ratio_reserved = math::Vec4(-1.0f, 1.7f, 0.0f, 0.0f);
     math::Mat4x4 inversed_rotation;
     math::Mat4x4 view;
     math::Mat4x4 projection;
