05190e7f9d953c003a85263524afecc6d7c7f28b
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2019-04-30 16:44:17 +0430

    fbp:: build:: setup:: --working on setup script and build--

--- setup.py
+++ setup.py
@@ -5,22 +5,37 @@ import os
 import subprocess
 import shutil
 import multiprocessing
+import platform
 from urllib.request import urlretrieve
 from zipfile import ZipFile
 
+PLATFORM = platform.system()
+print("Building in platform:", PLATFORM)
+
 SRC_PATH = os.path.dirname(os.path.abspath(__file__))
+os.chdir(SRC_PATH)
 
-SDK_PATH = SRC_PATH + "/sdk/"
+print("Updaating the git sub modules ...")
+subprocess.run([
+    'git', 
+    'submodule', 
+    'update',
+    '--init',
+    '--recursive', 
+    '--remote'
+])
 
+SDK_PATH = os.path.join(SRC_PATH, 'sdk')
 if not os.path.exists(SDK_PATH):
     os.makedirs(SDK_PATH)
 
 SDL2_DIR_NAME = 'SDL2-2.0.9'
-SDL2_DIR_PATH = SDK_PATH + SDL2_DIR_NAME
-SDL2_BUILD_DIR_PATH = SDK_PATH + 'sdl2-build'
+SDL2_DIR_PATH = os.path.join(SDK_PATH, SDL2_DIR_NAME)
+SDL2_BUILD_DIR_PATH = os.path.join(SDK_PATH, 'sdl2-build')
 SDL2_PACK_NAME = SDL2_DIR_NAME + '.zip'
 SDL2_PACK_URL = 'https://www.libsdl.org/release/' + SDL2_PACK_NAME
-SDL2_PACK_PATH = SDK_PATH + SDL2_PACK_NAME
+SDL2_PACK_PATH = os.path.join(SDK_PATH, SDL2_PACK_NAME)
+SDL2_BUILD_TYPE = 'MinSizeRel'
 
 
 def report_hook(blocknum, blocksize, totalsize):
@@ -53,16 +68,50 @@ else:
 
 if os.path.exists(SDL2_BUILD_DIR_PATH):
     shutil.rmtree(SDL2_BUILD_DIR_PATH)
-
 os.makedirs(SDL2_BUILD_DIR_PATH)
 os.chdir(SDL2_BUILD_DIR_PATH)
+
+LIBS_PATH = os.path.join(SDK_PATH, 'libs')
+if os.path.exists(LIBS_PATH):
+    shutil.rmtree(LIBS_PATH)
+os.makedirs(LIBS_PATH)
+
 subprocess.run([
     'cmake',
     '-B' + SDL2_BUILD_DIR_PATH,
     '-H' + SDL2_DIR_PATH,
-    '-DCMAKE_BUILD_TYPE=MinSizeRel'
+    '-DCMAKE_BUILD_TYPE=' + SDL2_BUILD_TYPE,
 ])
-subprocess.run([
-    'make',
-    '-j' + str(multiprocessing.cpu_count())
-])
+
+if 'Windows' in PLATFORM:
+    print('Building with Visual Studio')
+    SDL2_STATIC = 'SDL2-static'
+    SDL2_MAIN = 'SDL2main'
+    SDL2_STATIC_LIB = SDL2_STATIC + '.lib'
+    SDL2_MAIN_LIB = SDL2_MAIN + '.lib'
+    subprocess.run([
+        'msbuild',
+        SDL2_STATIC + '.vcxproj',
+        '/p:configuration=' + SDL2_BUILD_TYPE,
+        '/p:platform=x64'
+    ])
+    subprocess.run([
+        'msbuild',
+        SDL2_MAIN + '.vcxproj',
+        '/p:configuration=' + SDL2_BUILD_TYPE,
+        '/p:platform=x64'
+    ])
+    SDL2_BUILD_LIB_DIR_PATH = os.path.join(
+        SDL2_BUILD_DIR_PATH, SDL2_BUILD_TYPE)
+    shutil.move(
+        os.path.join(SDL2_BUILD_LIB_DIR_PATH, SDL2_STATIC_LIB),
+        os.path.join(LIBS_PATH, SDL2_STATIC_LIB))
+    shutil.move(
+        os.path.join(SDL2_BUILD_LIB_DIR_PATH, SDL2_MAIN_LIB),
+        os.path.join(LIBS_PATH, SDL2_MAIN_LIB))
+
+elif 'Linux' in PLATFORM:
+    subprocess.run([
+        'make',
+        '-j' + str(multiprocessing.cpu_count())
+    ])
