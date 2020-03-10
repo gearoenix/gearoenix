a3c0a83bda2350586444afafb92e6622a10ede1b
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-03-10 16:26:42 +0330

    reflection:: --reflection class changed--

--- gearoenix/render/reflection/rnd-rfl-reflection.hpp
+++ gearoenix/render/reflection/rnd-rfl-reflection.hpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 namespace gearoenix::physics::collider {
-class Sphere;
+class Aabb;
 }
 
 namespace gearoenix::render::engine {
@@ -19,13 +19,16 @@ class TextureCube;
 }
 
 namespace gearoenix::render::reflection {
+/// The influencing length indicate the length of the AABB of collider that if any object place (exactly) in it,
+/// the pbr materials of that object will get the properties of colliding reflection.
 class Reflection : public core::asset::Asset {
     GX_GET_CVAL_PRT(Type, reflection_type)
     GX_GETSET_VAL_PRT(bool, enabled, true)
     GX_GET_CPTR_PRT(engine::Engine, e)
-    GX_GET_UCPTR_PRT(physics::collider::Sphere, collider)
+    GX_GET_UCPTR_PRT(physics::collider::Aabb, collider)
     GX_GET_CREF_PRT(std::shared_ptr<texture::TextureCube>, irradiance)
     GX_GET_CREF_PRT(std::shared_ptr<texture::TextureCube>, radiance)
+    GX_GET_VAL_PRT(double, influencing_length, 5.0)
     GX_GETSET_VAL_PRT(bool, is_dynamic, false)
 protected:
     Reflection(
