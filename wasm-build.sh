31fd3e829f10fe8fd8f430feb4863ebf86a5f0b1
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-06-03 11:26:04 +0430

    ci:: --circle ci can not handle 8 parallel compilation--

--- wasm-build.sh
+++ wasm-build.sh
@@ -4,4 +4,4 @@ mkdir -p assets
 cd wasm-build || exit
 source "$EMSDK"/emsdk_env.sh
 emcmake cmake -DCMAKE_BUILD_TYPE=MinRelSize -DGX_ENABLE_DEMO_WORLD_WIDE_RACING=ON ..
-cmake --build . --config MinRelSize --parallel 8  # "$(nproc --all)"
+cmake --build . --config MinRelSize --parallel 4  # "$(nproc --all)"
