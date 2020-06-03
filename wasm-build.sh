6c6789bd26e58cfb23b7344c053dd0a07fac0e06
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-06-03 12:06:43 +0430

    ci:: --trying to solve ci problem--

--- wasm-build.sh
+++ wasm-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p wasm-build
 mkdir -p assets
+echo "Just a placeholder" > assets/placeholder
 cd wasm-build || exit
 source "$EMSDK"/emsdk_env.sh
 emcmake cmake -DCMAKE_BUILD_TYPE=MinRelSize -DGX_ENABLE_DEMO_WORLD_WIDE_RACING=ON ..
