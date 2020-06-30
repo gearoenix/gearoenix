15829ff692e7c662526f1c904f3351db4a066995
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-06-30 15:12:28 +0430

    pathfinding:: --working on dijkstra--

--- gearoenix/ai/gx-ai-dijkstra.cpp
+++ gearoenix/ai/gx-ai-dijkstra.cpp
@@ -1,8 +1,49 @@
 #include "gx-ai-dijkstra.hpp"
+#include <limits>
+#include <optional>
 
-gearoenix::ai::Dijkstra::Dijkstra(std::vector<std::map<int, int>> graph) noexcept
-    : graph(std::move(graph))
+gearoenix::ai::Dijkstra::Dijkstra(const std::vector<std::vector<std::tuple<int, int>>>& g) noexcept
 {
+    graph.reserve(g.size());
+    for (auto& c : g) {
+        std::map<int, int> v;
+        for (auto [i, w] : c) {
+            v[w] = i;
+        }
+        graph.emplace_back(std::move(v));
+    }
+}
+
+gearoenix::ai::Graph<int, int> gearoenix::ai::Dijkstra::best_graph(const int start) const noexcept
+{
+    struct SeenInfo {
+        int weight = std::numeric_limits<int>::max();
+        bool not_best = true;
+    };
+    std::vector<SeenInfo> seen_vertices;
+    seen_vertices.resize(graph.size());
+    seen_vertices[start] = SeenInfo { .weight = 0, .not_best = true };
+    int idx = start;
+    for (std::size_t i = 0; i < graph.size(); ++i) {
+        int min_w = std::numeric_limits<int>::max();
+        for (std::size_t j = 0; j < graph.size(); ++j) {
+            const auto& v = seen_vertices[j];
+            if (v.not_best && v.weight < min_w) {
+                min_w = v.weight;
+                idx = j;
+            }
+        }
+        seen_vertices[idx].not_best = false;
+        const auto& connections = graph[idx];
+        for (const auto [cw, ci] : connections) {
+            auto& c = seen_vertices[ci];
+            int new_w = cw + min_w;
+            if (c.weight > new_w && c.not_best) {
+                c.weight = new_w;
+            }
+        }
+    }
+    return gearoenix::ai::Graph<int, int>();
 }
 
 gearoenix::ai::Dijkstra::~Dijkstra() noexcept = default;
--- gearoenix/ai/gx-ai-dijkstra.hpp
+++ gearoenix/ai/gx-ai-dijkstra.hpp
@@ -1,17 +1,22 @@
 #ifndef GEAROENIX_AI_DIJKSTRA_HPP
 #define GEAROENIX_AI_DIJKSTRA_HPP
-
+#include "gx-ai-graph.hpp"
 #include <map>
+#include <tuple>
 #include <utility>
 #include <vector>
 
 namespace gearoenix::ai {
 class Dijkstra {
+    /// vertex->weight->index
     std::vector<std::map<int, int>> graph;
 
 public:
-    Dijkstra(std::vector<std::map<int, int>> graph) noexcept;
+    /// vertices -> connections -> (index, weight)
+    Dijkstra(const std::vector<std::vector<std::tuple<int, int>>>& graph) noexcept;
     ~Dijkstra() noexcept;
+
+    Graph<int, int> best_graph(int start) const noexcept;
 };
 }
 
