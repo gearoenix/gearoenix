47a5d8fb45da0d12da737f1a47b22855023dc5ea
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-08-01 14:09:08 +0430

    wip:: working on bvh

--- gearoenix/physics/accelerator/phs-acc-bvh.hpp
+++ gearoenix/physics/accelerator/phs-acc-bvh.hpp
@@ -21,70 +21,34 @@ class Collider;
 }
 
 namespace gearoenix::physics::accelerator {
-class Bvh {
+struct Bvh {
+    constexpr static const std::size_t bins_count = 32;
+    constexpr static const std::size_t walls_count = bins_count - 1;
 private:
-    class Node {
-    public:
-        enum struct Type {
-            INTERNAL,
-            LEAF,
-        };
-        GX_GET_CVAL_PRT(Type, node_type)
-        GX_GET_REF_PRT(math::Aabb3, volume)
-    protected:
-        explicit Node(const Type t) noexcept
-            : node_type(t)
-        {
-        }
-        Node(const Type t, math::Aabb3 volume) noexcept
-            : node_type(t)
-            , volume(std::move(volume))
-        {
-        }
-
-    public:
-        virtual ~Node() noexcept = default;
-        [[nodiscard]] virtual std::string to_string() const noexcept;
-        [[nodiscard]] virtual std::optional<std::pair<double, collider::Collider*>> hit(const math::Ray3& r, double d_min) const noexcept;
-        virtual void call_on_intersecting(const collider::Collider* cld, const std::function<void(collider::Collider* const cld)>& collided) const noexcept = 0;
-        virtual void map(const std::function<void(collider::Collider* const cld)>& collided) const noexcept = 0;
+    struct Bin {
+        math::Aabb3 volume;
+        std::vector<collider::Collider*> colliders;
     };
 
-    class LeafNode : public Node {
-        GX_GET_REF_PRV(std::vector<collider::Collider*>, colliders)
-    public:
-        LeafNode(std::vector<collider::Collider*> colliders, const math::Aabb3& volume) noexcept
-            : Node(Type::LEAF, volume)
-            , colliders(std::move(colliders))
-        {
-        }
-        [[nodiscard]] std::optional<std::pair<double, collider::Collider*>> hit(const math::Ray3& r, double d_min) const noexcept final;
-        [[nodiscard]] std::string to_string() const noexcept final;
-        void call_on_intersecting(const collider::Collider* cld, const std::function<void(collider::Collider* const cld)>& collided) const noexcept final;
-        void map(const std::function<void(collider::Collider* const cld)>& collided) const noexcept final;
+    struct Node {
+        math::Aabb3 volume;
+        std::vector<collider::Collider*> colliders;
+        std::size_t left_index = 0;
+        Node * left = nullptr;
+        std::size_t right_index = 0;
+        Node * right = nullptr;
+
+        [[nodiscard]] std::string to_string() const noexcept;
+        [[nodiscard]] std::optional<std::pair<double, collider::Collider*>> hit(const math::Ray3& r, double d_min) const noexcept;
+        void call_on_intersecting(const collider::Collider* cld, const std::function<void(collider::Collider* const cld)>& collided) const noexcept;
+        void map(const std::function<void(collider::Collider* const cld)>& collided) const noexcept;
+        void init(const std::vector<collider::Collider*>& colliders, const math::Aabb3& volume) noexcept;
     };
 
-    class InternalNode : public Node {
-        GX_GET_UPTR_PRV(Node, left)
-        GX_GET_UPTR_PRV(Node, right)
-
-    private:
-        void init(const std::vector<collider::Collider*>& colliders) noexcept;
-
-    public:
-        explicit InternalNode(const std::vector<collider::Collider*>& colliders) noexcept;
-        InternalNode(const std::vector<collider::Collider*>& colliders, const math::Aabb3& volume) noexcept;
-        [[nodiscard]] std::optional<std::pair<double, collider::Collider*>> hit(const math::Ray3& r, double d_min) const noexcept final;
-        [[nodiscard]] std::string to_string() const noexcept final;
-        void call_on_intersecting(const collider::Collider* cld, const std::function<void(collider::Collider* const cld)>& collided) const noexcept final;
-        void map(const std::function<void(collider::Collider* const cld)>& collided) const noexcept final;
-    };
-
-    std::unique_ptr<InternalNode> root;
+    std::vector<Node> nodes;
+    Bin bins[bins_count];
 
 public:
-    Bvh() noexcept = default;
-    ~Bvh() noexcept = default;
     void reset(const std::vector<collider::Collider*>& colliders) noexcept;
     [[nodiscard]] std::optional<std::pair<double, collider::Collider*>> hit(const math::Ray3& r, double d_min) const noexcept;
     [[nodiscard]] std::string to_string() const noexcept;
