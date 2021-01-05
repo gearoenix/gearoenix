ae640fbd7beeea73280eec822a08735591f26dcc
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2021-01-05 15:19:46 +0330

    ci:: windows::

--- scripts/powershell/install-vulkan-sdk.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorView = 'NormalView'
-New-Item -ItemType Directory -Force -Path build
-Set-Location -Path build
-Invoke-WebRequest "https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe" -OutFile build\vulkan-sdk.exe
-.\build\vulkan-sdk.exe /S
