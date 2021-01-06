38632bccd552daf8160e1367d17e8c9f586f25d9
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2021-01-06 12:14:10 +0330

    ci:: linux:: test::

---
+++ tests/gx-test-cr-ecs.hpp
@@ -0,0 +1,10 @@
+#ifndef GEAROENIX_TEST_CORE_ECS_HPP
+#define GEAROENIX_TEST_CORE_ECS_HPP
+#include <gearoenix/core/ecs/gx-cr-ecs-world.hpp>
+
+BOOST_AUTO_TEST_CASE(gearoenix_core_ecs_test)
+{
+
+}
+
+#endif
