dcd47ec36352aff440f0588814db5ffa5b051168
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2021-02-15 14:27:28 +0330

    scene:: --working on node hierarchy of the scene--

--- gearoenix/render/gltf/gx-rnd-gltf-node-manager.cpp
+++ gearoenix/render/gltf/gx-rnd-gltf-node-manager.cpp
@@ -2,6 +2,36 @@
 #include "gx-rnd-gltf-loader.hpp"
 #include <tiny_gltf.h>
 
+gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get_mesh(
+    const std::size_t index, const gearoenix::core::sync::EndCallerIgnored& c) noexcept
+{
+    const auto& data = *loader.get_data();
+    const auto& nodes = data.nodes;
+    const auto& node = nodes[index];
+    const auto& mesh = data.meshes[node.mesh];
+    mesh.
+}
+
+gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get_camera(
+    const std::size_t, const gearoenix::core::sync::EndCallerIgnored&) noexcept {
+    GX_UNIMPLEMENTED
+}
+
+gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get_camera(
+    const std::size_t, const std::size_t, const gearoenix::core::sync::EndCallerIgnored&) noexcept {
+    GX_UNIMPLEMENTED
+}
+
+gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get_skin(
+    const std::size_t, const gearoenix::core::sync::EndCallerIgnored&) noexcept {
+    GX_UNIMPLEMENTED
+}
+
+gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get_light(
+    const std::size_t, const std::size_t, const gearoenix::core::sync::EndCallerIgnored&) noexcept {
+    GX_UNIMPLEMENTED
+}
+
 gearoenix::render::gltf::NodeManager::NodeManager(Loader& loader) noexcept
     : loader(loader)
 {
@@ -10,7 +40,6 @@ gearoenix::render::gltf::NodeManager::NodeManager(Loader& loader) noexcept
 gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get(
     const std::size_t index, const core::sync::EndCallerIgnored& c) noexcept
 {
-
     auto index_search = loaded_nodes.find(index);
     if (loaded_nodes.end() != index_search)
         return index_search->second;
@@ -19,7 +48,39 @@ gearoenix::core::ecs::Entity::id_t gearoenix::render::gltf::NodeManager::get(
     const auto& nodes = data.nodes;
     const auto& node = nodes[index];
 
-    GX_UNIMPLEMENTED
+    core::ecs::Entity::Builder entity_builder;
 
-    return 0;
+    const auto id = entity_builder.get_id();
+    entity_builder.set_name(node.name);
+
+    loaded_nodes.emplace(index, entity_builder.get_id());
+
+    if (node.mesh >= 0) {
+        get_mesh(index, std::move(entity_builder), c);
+        return id;
+    }
+
+    if (node.camera >= 0)
+        return get_camera(index, c);
+
+    if (node.skin >= 0)
+        return get_skin(index, c);
+
+    GX_CHECK_NOT_EQUAL_D(node.children.size(), 1)
+
+    const std::size_t child_node_index = node.children[0];
+    auto child_node_search = loaded_nodes.find(child_node_index);
+    GX_CHECK_EQUAL_D(loaded_nodes.end(), child_node_search)
+    const auto& child_node = nodes[child_node_index];
+
+    if (child_node.camera >= 0)
+        return get_camera(index, child_node_index, c);
+
+    GX_CHECK_EQUAL_D(child_node.mesh, -1)
+    GX_CHECK_EQUAL_D(child_node.skin, -1)
+
+    if (child_node.extensions.contains("KHR_lights_punctual"))
+        return get_light(index, child_node_index, c);
+
+    GX_UNEXPECTED
 }
--- gearoenix/render/gltf/gx-rnd-gltf-node-manager.hpp
+++ gearoenix/render/gltf/gx-rnd-gltf-node-manager.hpp
@@ -10,7 +10,12 @@ struct NodeManager final {
 private:
     Loader& loader;
     std::map<std::size_t, core::ecs::Entity::id_t> loaded_nodes;
-    std::map<std::string, core::ecs::Entity::id_t> loaded_nodes_names;
+
+    [[nodiscard]] core::ecs::Entity::id_t get_mesh(std::size_t, const core::sync::EndCallerIgnored& c) noexcept;
+    [[nodiscard]] core::ecs::Entity::id_t get_camera(std::size_t, const core::sync::EndCallerIgnored& c) noexcept;
+    [[nodiscard]] core::ecs::Entity::id_t get_camera(std::size_t, std::size_t, const core::sync::EndCallerIgnored& c) noexcept;
+    [[nodiscard]] core::ecs::Entity::id_t get_skin(std::size_t, const core::sync::EndCallerIgnored& c) noexcept;
+    [[nodiscard]] core::ecs::Entity::id_t get_light(std::size_t, std::size_t, const core::sync::EndCallerIgnored& c) noexcept;
 
 public:
     explicit NodeManager(Loader& loader) noexcept;
