dacf25f86740b50c8c1dea6f18b2f2a0f90e5c45
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2021-03-14 23:50:01 +0330

    vulkan::
     - only a small change in the main shader, still thinking on the
       upcomming design, it is taking long time, and my time is short it is
       better to start from some where.

--- gearoenix/vulkan/shader/gx-vk-shd-pbr-close-hit.glsl
+++ gearoenix/vulkan/shader/gx-vk-shd-pbr-close-hit.glsl
@@ -4,22 +4,39 @@
 #extension GL_EXT_scalar_block_layout : enable
 #extension GL_GOOGLE_include_directive : enable
 
-#include "binding.glsl"
-#include "gltf.glsl"
-#include "raycommon.glsl"
+#include "gx-vk-shd-bindings.hpp"
 
-hitAttributeEXT vec2 attribs;
+struct Vertex {
+    vec3 position;
+    vec3 normal;
+    vec4 tangent;
+    vec2 uv;
+};
 
-// clang-format off
+struct Material {
+    uint albedo_texture_index;
+    uint normal_texture_index;
+    uint metalic_roughness_texture_index;
+    uint emission_texture_index;
+    uint ambient_oclussion_texture_index;
+};
+
+struct Mesh {
+    uint vertex_index;
+    uint index_index;
+    uint material_index;
+};
+
+hitAttributeEXT vec2 attributes;
+
 layout(location = 0) rayPayloadInEXT hitPayload prd;
 layout(location = 1) rayPayloadEXT bool isShadowed;
 
-layout(set = 0, binding = 0 ) uniform accelerationStructureEXT topLevelAS;
-layout(set = 0, binding = 2) readonly buffer _InstanceInfo {PrimMeshInfo primInfo[];};
 
-layout(set = 1, binding = B_VERTICES) readonly buffer _VertexBuf {float vertices[];};
-layout(set = 1, binding = B_INDICES) readonly buffer _Indices {uint indices[];};
-layout(set = 1, binding = B_NORMALS) readonly buffer _NormalBuf {float normals[];};
-layout(set = 1, binding = B_TEXCOORDS) readonly buffer _TexCoordBuf {float texcoord0[];};
-layout(set = 1, binding = B_MATERIALS) readonly buffer _MaterialBuffer {GltfShadeMaterial materials[];};
-layout(set = 1, binding = B_TEXTURES) uniform sampler2D texturesMap[];
+layout(set = 1, binding = GX_VK_BIND_RAY_VERTICES) readonly buffer VerticisBuffer { Vertex vertices[]; };
+layout(set = 1, binding = GX_VK_BIND_RAY_INDICES) readonly buffer IndicesBuffer { uint indices[]; };
+layout(set = 1, binding = GX_VK_BIND_RAY_MATERIALS) readonly buffer MaterialBuffer { Material materials[]; };
+layout(set = 1, binding = GX_VK_BIND_RAY_MESH) readonly buffer MeshBuffer { Mesh meshes[]; };
+layout(set = 0, binding = GX_VK_BIND_RAY_TLAS) uniform accelerationStructureEXT tlas;
+layout(set = 1, binding = GX_VK_BIND_RAY_2D_TEXTURES) uniform sampler2D textures[];
+layout(set = 1, binding = GX_VK_BIND_RAY_CUBE_TEXTURES) uniform samplerCube cube_textures[];
