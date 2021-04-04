ec1cbd02c089cfc6cf5c693aa9b8680f785eeb77
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2021-04-04 18:00:09 +0100

    vulkan:: shader::
     - shader manager strucute has been implemented.

---
+++ gearoenix/vulkan/shader/gx-vk-shd-manager.hpp
@@ -0,0 +1,32 @@
+#ifndef GEAROENIX_VULKAN_SHADER_MANAGER_HPP
+#define GEAROENIX_VULKAN_SHADER_MANAGER_HPP
+#include "../../render/gx-rnd-build-configuration.hpp"
+#ifdef GX_RENDER_VULKAN_ENABLED
+#include <map>
+#include <memory>
+#include <string>
+
+namespace gearoenix::vulkan::engine {
+struct Engine;
+}
+
+namespace gearoenix::vulkan::shader {
+struct Module;
+struct Manager final {
+private:
+    std::map<std::string, std::weak_ptr<Module>> modules;
+    const engine::Engine& e;
+
+public:
+    Manager(Manager&&) = delete;
+    Manager(const Manager&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    Manager& operator=(const Manager&) = delete;
+    explicit Manager(const engine::Engine& e) noexcept;
+    ~Manager() noexcept;
+    [[nodiscard]] std::shared_ptr<Module> get(const std::string& shader_name) noexcept;
+};
+}
+
+#endif
+#endif
