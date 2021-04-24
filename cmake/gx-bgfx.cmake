cfd24ee476de504be18cbaa230b2b55ba83f4c20
Thany <hossein.noroozpour@gmail.com>
2021-04-24 04:00:37 +0100

    bgfx::
     - adding bgfx support to the cmake

---
+++ cmake/gx-bgfx.cmake
@@ -0,0 +1,40 @@
+FUNCTION(GX_PREPARE_BGFX)
+    IF(GX_BGFX_ENABLED)
+        SET(GX_BGFX_BASE_PATH "${GX_SDK_PATH}/bgfx")
+        SET(GX_BGFX_PATH "${GX_BGFX_BASE_PATH}/bgfx")
+        SET(GX_BGFX_BX_PATH "${GX_BGFX_BASE_PATH}/bx")
+        SET(GX_BGFX_BIMG_PATH "${GX_BGFX_BASE_PATH}/bimg")
+        GX_DOWNLOAD_FILE("https://github.com/bkaradzic/bgfx/archive/master.zip" "${GX_BGFX_PATH}")
+        GX_DOWNLOAD_FILE("https://github.com/bkaradzic/bx/archive/master.zip" "${GX_BGFX_BX_PATH}")
+        GX_DOWNLOAD_FILE("https://github.com/bkaradzic/bimg/archive/master.zip" "${GX_BGFX_BIMG_PATH}")
+        SET(GX_BGFX_INCLUDE_DIRS ${GX_BGFX_PATH}/include)
+        INCLUDE_DIRECTORIES(${GX_BGFX_INCLUDE_DIRS})
+        SET(GX_BGFX_BUILD_PATH ${GX_BGFX_PATH}/.build)
+        IF(NOT EXISTS ${GX_BGFX_BUILD_PATH})
+            IF(APPLE)
+                SET(GX_BGFX_GENIE_EXE "${GX_BGFX_BX_PATH}/tools/bin/darwin/genie")
+                SET(GX_BGFX_GENIE_ARG "xcode")
+            ELSEIF(LINUX)
+                SET(GX_BGFX_GENIE_EXE "${GX_BGFX_BX_PATH}/tools/bin/linux/genie")
+                SET(GX_BGFX_GENIE_ARG "gmake")
+            ELSEIF(MSVC)
+                SET(GX_BGFX_GENIE_EXE "${GX_BGFX_BX_PATH}/tools/bin/windows/genie.exe")
+                SET(GX_BGFX_GENIE_ARG "vs2019")
+                EXECUTE_PROCESS(COMMAND ${GX_BGFX_GENIE_EXE} ${GX_BGFX_GENIE_ARG} WORKING_DIRECTORY ${GX_BGFX_PATH})
+                SET(GX_BGFX_SLN_PATH "${GX_BGFX_BUILD_PATH}/projects/vs2019")
+                EXECUTE_PROCESS(
+                        COMMAND "${GX_MSBUILD_EXE}" "bgfx.sln" "-t:Rebuild" "-p:Configuration=Release" "-p:Platform=x64"
+                        WORKING_DIRECTORY "${GX_BGFX_SLN_PATH}")
+                SET(GX_BGFX_BIN_DIR ${GX_BGFX_BUILD_PATH}/win64_vs2019/bin)
+                SET(GX_LIBS ${GX_LIBS}
+                        ${GX_BGFX_BIN_DIR}/bgfxRelease.lib
+                        # ${GX_BGFX_BIN_DIR}/bimg_decodeRelease.lib
+                        # ${GX_BGFX_BIN_DIR}/bimgRelease.lib
+                        # ${GX_BGFX_BIN_DIR}/bxRelease.lib
+                        PARENT_SCOPE)
+            ELSE()
+                MESSAGE(FATAL_ERROR "Unsupported platform")
+            ENDIF()
+        ENDIF()
+    ENDIF()
+ENDFUNCTION()
