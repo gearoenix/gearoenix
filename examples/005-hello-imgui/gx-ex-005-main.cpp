aadf06711183414d84a4d42ab664f1991b5627d8
Thany <Hossein.Noroozpour@gmail.com>
2025-05-10 23:43:26 +0100

    example005 is now working

--- examples/005-hello-imgui/gx-ex-005-main.cpp
+++ examples/005-hello-imgui/gx-ex-005-main.cpp
@@ -2,35 +2,31 @@
 #include <imgui.h>
 
 struct GameApp final : gearoenix::core::Application {
-    explicit GameApp(gearoenix::platform::Application& plt_app) noexcept;
-    void update() override;
-};
+private:
+    bool show_demo_window = true;
+    bool show_hello_window = true;
 
-GameApp::GameApp(gearoenix::platform::Application& plt_app) noexcept
-    : Application(plt_app)
-{
-}
+public:
+    GameApp() = default;
 
-void GameApp::update()
-{
-    static bool show_demo_window = true;
-    static bool show_hello_window = true;
+    void update() override
+    {
+        Application::update();
 
-    Application::update();
-
-    if (show_demo_window) {
-        ImGui::ShowDemoWindow(&show_demo_window);
-    }
-
-    if (show_hello_window) {
-        if (ImGui::Begin("Hello Window!", &show_hello_window)) {
-            ImGui::Text("Hello Label!");
-            if (ImGui::Button("Close")) {
-                show_hello_window = false;
-            }
+        if (show_demo_window) {
+            ImGui::ShowDemoWindow(&show_demo_window);
         }
-        ImGui::End();
+
+        if (show_hello_window) {
+            if (ImGui::Begin("Hello Window!", &show_hello_window)) {
+                ImGui::Text("Hello Label!");
+                if (ImGui::Button("Close")) {
+                    show_hello_window = false;
+                }
+            }
+            ImGui::End();
+        }
     }
-}
+};
 
 GEAROENIX_START(GameApp)
